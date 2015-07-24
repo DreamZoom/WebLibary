44e10a92b70f7e62d4dfbcbd05a58797c98aa506
王小龙 <1036199213@qq.com>
2015-07-24 09:58:15 +0800

    weblibary

---
+++ MvcLibary/MvcLibary/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcLibary.MvcApplication" Language="C#" %>
---
+++ MvcLibary/WebLibrary/BLLService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace WebLibrary
+{
+    /// <summary>
+    /// 业务服务基类
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class BLLService<T>
+        where T : class
+    {
+        /// <summary>
+        /// 数据访问上下文
+        /// </summary>
+        protected DbContext DbContext { get; set; }
+
+        public BLLService(DbContext dbcontext)
+        {
+            DbContext = dbcontext;
+        }
+
+        public bool Add(T model)
+        {
+            DbContext.Entry<T>(model).State = EntityState.Added;
+            return DbContext.SaveChanges() > 0;
+        }
+
+        public bool Delete(T model)
+        {
+            DbContext.Entry<T>(model).State = EntityState.Deleted;
+            return DbContext.SaveChanges() > 0;
+        }
+
+        public bool Update(T model)
+        {
+            DbContext.Entry<T>(model).State = EntityState.Modified;
+            return  DbContext.SaveChanges()>0;
+           
+        }
+
+    }
+}
