34e0f001a6cb18a0c5dedc6ca31f6afebfb4d6b0
王小龙 <1036199213@qq.com>
2015-09-08 14:37:40 +0800

    表格

--- MvcLibary/MvcLibary/Views/Home/Index.aspx
+++ MvcLibary/MvcLibary/Views/Home/Index.aspx
@@ -5,30 +5,26 @@
 </asp:Content>
 
 <asp:Content ID="indexFeatured" ContentPlaceHolderID="FeaturedContent" runat="server">
-    <section class="featured">
-        <div class="content-wrapper">
-            <hgroup class="title">
-                <h1>Home Page.</h1>
-                <h2><%: ViewBag.Message %></h2>
-            </hgroup>
-            <p>
-                To learn more about ASP.NET MVC visit
-                <a href="http://asp.net/mvc" title="ASP.NET MVC Website">http://asp.net/mvc</a>.
-                The page features <mark>videos, tutorials, and samples</mark> to help you get the most from ASP.NET MVC.
-                If you have any questions about ASP.NET MVC visit
-                <a href="http://forums.asp.net/1146.aspx/1?MVC" title="ASP.NET MVC Forum">our forums</a>.
-            </p>
-        </div>
-    </section>
+    
 </asp:Content>
 
 <asp:Content ID="indexContent" ContentPlaceHolderID="MainContent" runat="server">
     <% List<ListItem> list = new List<ListItem>();
-       
-       for (var i = 0; i < 20;i++ )
+
+       for (var i = 0; i < 20; i++)
        {
-            list.Add(new ListItem() { Text ="哈哈哈"+i, Value ="呵呵呵"+i });
+           list.Add(new ListItem() { Text = "哈哈哈" + i, Value = "呵呵呵" + i });
        }
-        %>
-   <%:Html.EasyUIGird(list) %>
+    %>
+    <%:Html.EasyUIGrid(list) %>
+
+
+    <ul class="easyui-datalist" title="DataList" style="width: 400px; height: 250px" chexkbox="true">
+        <li value="AL">Alabama</li>
+        <li value="AK">Alaska</li>
+        <li value="AZ">Arizona</li>
+        <li value="AR">Arkansas</li>
+        <li value="CA">California</li>
+        <li value="CO">Colorado</li>
+    </ul>
 </asp:Content>
--- MvcLibary/WebLibrary/HtmlHelper/EasyUIGirdExtensions.cs
+++ MvcLibary/WebLibrary/HtmlHelper/EasyUIGirdExtensions.cs
@@ -7,22 +7,48 @@ using System.Reflection;
 
 namespace System.Web.Mvc
 {
+    /// <summary>
+    /// 表格参数设置
+    /// </summary>
+    public class EasyUIGridOptions
+    {
+        public bool ShowCheckBox { get; set; }
+    }
     public static class EasyUIGirdExtensions
     {
-        public static MvcHtmlString EasyUIGird<T>(this HtmlHelper helper,IEnumerable<T> list)
+        public static MvcHtmlString EasyUIGrid<T>(this HtmlHelper helper,IEnumerable<T> list,EasyUIGridOptions options=null)
         {
+            #region 设置默认参数
+            if (options==null)
+            {
+                options = new EasyUIGridOptions()
+                {
+                     ShowCheckBox = true
+                };
+            }
+            #endregion
+
             var propertys = typeof(T).GetProperties();
 
+            #region 表头
             List<string> headers = propertys.ToList().ConvertAll(m => getFiledName(m));
 
+            if (options.ShowCheckBox)
+            {
+                headers.Add(string.Format("<th field=\"{0}\" checkbox=\"true\"></th>", "ck"));
+            }
             string tHead =string.Format("<thead><tr>{0}</tr></thead>", string.Join("",headers));
 
+            #endregion
+
             StringBuilder tBody = new StringBuilder();
-            tBody.AppendLine("<table class=\"easyui-datagird\">");
+            tBody.AppendLine("<table class=\"easyui-datagrid\" fit=\"false\" pagination=\"true\" >");
              tBody.AppendLine(tHead);
             tBody.AppendLine("<tbody>");
             foreach(var model in list){
-                tBody.AppendLine(string.Format("<tr>{0}</tr>", string.Join("",propertys.ToList().ConvertAll(m => getFiledValue(m,model)))));
+                List<string> tds = propertys.ToList().ConvertAll(m => getFiledValue(m,model));
+                tds.Add(string.Format("<td>{0}</td>", ""));
+                tBody.AppendLine(string.Format("<tr>{0}</tr>", string.Join("", tds)));
             }
             tBody.AppendLine("</tbody>");
              tBody.AppendLine("</table>");
@@ -31,7 +57,7 @@ namespace System.Web.Mvc
 
         private static string getFiledName(PropertyInfo property)
         {
-            return string.Format("<th>{0}</th>",property.Name);
+            return string.Format("<th field=\"{0}\">{0}</th>", property.Name);
         }
 
         private static string getFiledValue(PropertyInfo property,object model)
